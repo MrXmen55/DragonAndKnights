9d3d706ea724f1f2886bfdd9bb1a49b1ac3a0e29
petelind <d.petelin@icloud.com>
2017-03-22 18:03:35 +0200

    Добавлен пример с семейством волков

---
+++ WolfPack/WolfPack/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WolfPack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Wolf> thePack = new List<Wolf>();
+
+            Wolf jackie = new Wolf("Jackie");
+            thePack.Add(jackie);
+
+            Wolf john = new Wolf("John");
+            thePack.Add(john);
+
+            foreach (Wolf wolf in thePack)
+            {
+                wolf.Howl();
+            }
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ WolfPack/WolfPack/Wolf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WolfPack
+{
+    class Wolf
+    {
+        public string Name;
+        public Wolf(string name)
+        {
+            Name = name;
+        }
+
+        public void Howl()
+        {
+            Console.WriteLine(Name + " says: UUUUUUUUUUU!...");
+        }
+    }
+}
